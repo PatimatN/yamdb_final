c1de2167c566afd420dd88c2d60637b6326cebf5
Патимат Нажмудинова <patimat.n.m@yandex.ru>
2021-06-29 10:36:47 +0300

    second commit

--- api_reviews/permissions.py
+++ api_reviews/permissions.py
@@ -5,6 +5,7 @@ class IsAdminOrReadOnly(BasePermission):
     def has_permission(self, request, view):
         if request.method in SAFE_METHODS or request.user.is_staff:
             return True
+        return False
 
 
 class IsOwnerOrReadOnlyPermission(BasePermission):
