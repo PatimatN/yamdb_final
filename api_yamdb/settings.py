55b55aec7e7d985d7edc90b037f9b91a5e8e0f3a
PatimatN <patimat.n.m@yandex.ru>
2021-06-30 12:33:09 +0300

    temp

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('SECRET_KEY_DJANGO', 'to_pass_tests')
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
